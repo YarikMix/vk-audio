de331ea0a61ec0b50100724d65441d008614c6dd
YarikMix <43493788+YarikMix@users.noreply.github.com>
2021-01-29 20:30:26 +0300

    Update get_last_vk_id.py

--- src/get_last_vk_id.py
+++ src/get_last_vk_id.py
@@ -7,13 +7,10 @@ HEADERS = {
 }
 
 
-def get_html(url, params=''):
-	return requests.get(url, headers=HEADERS, params=params)
-
-
-def get_next_url(html):
-	if html.status_code == 200:
-		soup = BeautifulSoup(html.content, 'html.parser')
+def get_next_url(url):
+	response = requests.get(url)
+	if response.status_code == 200:
+		soup = BeautifulSoup(response.content, 'html.parser')
 		url = "https://vk.com/" + soup.find("div", class_="catalog_wrap").find_all("div", class_="column4")[-1].find_all("a")[-1]["href"] 
 		return url
 	else:
@@ -24,12 +21,12 @@ def get_last_vk_id():
 	url = "https://vk.com/catalog.php"
 	while True:
 		try:
-			html = get_html(url)
-			url = get_next_url(html)
+			url = get_next_url(url)
 		except IndexError:
-			html = get_html(url)
-			user_id = "Error"
-			if html.status_code == 200:
-				soup = BeautifulSoup(html.content, 'html.parser')
+			response = requests.get(url)
+			if response.status_code == 200:
+				soup = BeautifulSoup(response.content, 'html.parser')
 				user_id = soup.find("div", class_="catalog_wrap").find_all("div", class_="column2")[-1].find_all("a")[-1]["href"][2:]
-			return int(user_id)
+				return int(user_id)
+			else:
+				return "Error"
